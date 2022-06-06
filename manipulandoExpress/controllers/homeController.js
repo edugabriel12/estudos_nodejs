ad4fd153028e32c9cbf74858c88cfbb7b99d708b
edugabriel12 <gabriellima4539@yahoo.com>
2022-06-05 21:26:27 -0300

    Commit manipulandoRoutes

---
+++ manipulandoExpress/controllers/homeController.js
@@ -0,0 +1,7 @@
+exports.paginaInicial = (req, res) => {
+  res.send(`
+  <form action="/" method="POST">
+  Nome do cliente: <input type="text" name="nome">
+  <button>Enviar</button>
+  </form>`)
+}
