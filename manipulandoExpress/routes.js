9ca554a50d5bb903a2278debf9360ab0ce645701
edugabriel12 <gabriellima4539@yahoo.com>
2022-06-08 16:42:30 -0300

    Updates

--- manipulandoExpress/routes.js
+++ manipulandoExpress/routes.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const homeController = require('./src/controllers/homeController')
 
 route.get('/', homeController.paginaInicial)
+route.post('/', homeController.trataPost)
 
 module.exports = route
--- manipulandoExpress/server.js
+++ manipulandoExpress/server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const path = require('path')
+const { middlewareGlobal } = require('./src/middlewares/middleware')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.use(middlewareGlobal)
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
