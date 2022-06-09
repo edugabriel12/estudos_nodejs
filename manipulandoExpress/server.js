599ca938cc4b12aea6a908431080a6aeab7eea06
edugabriel12 <gabriellima4539@yahoo.com>
2022-06-08 22:21:02 -0300

    More Updates

--- manipulandoExpress/server.js
+++ manipulandoExpress/server.js
@@ -1,5 +1,24 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.emit('conexao efetuada')
+  })
+  .catch(e => {
+    console.log(e)
+  })
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+
 const routes = require('./routes')
 const path = require('path')
 const { middlewareGlobal } = require('./src/middlewares/middleware')
@@ -7,11 +26,26 @@ const { middlewareGlobal } = require('./src/middlewares/middleware')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, 'public')))
 
+const sessionOptions = session({
+  secret: 'secret secret 123',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+})
+
 app.use(middlewareGlobal)
+app.use(sessionOptions)
+app.use(flash())
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
 app.use(routes)
 
-app.listen(3000)
+app.on('conexao efetuada', () => {
+  app.listen(3000)
+})
